3b539c9d232337eee2c24a6a664dc0710b6d7449
Komron <2402uah@gmail.com>
2023-03-11 10:06:16 +0500

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '4804e6dc57msh17bcf890ce5eacbp1e886fjsn51a32fbe704d',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
